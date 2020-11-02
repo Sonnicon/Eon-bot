88f02d11eaeb4d0e0a655fe592883f4f89a54375
Sonnicon <sonniconn@gmail.com>
2020-11-02 11:44:37 +0000

    Nicer startup

--- eon/modules/startup.groovy
+++ eon/modules/startup.groovy
@@ -1,8 +1,6 @@
 import sonnicon.eonbot.core.Modules
 
 static void main(arg) {
-    Modules.loadModule("permissions")
-    Modules.loadModule("modules")
-    Modules.loadModule("basiccommands")
-    Modules.loadModule("markov")
+    String[] modules = ["log", "permissions", "modules", "basiccommands", "markov"]
+    modules.each {Modules.loadModule(it)}
 }
